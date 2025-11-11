a6158d5d4e5cb9358cd6501f966dc868eac1c164
Anton Savinykh <freevolnost@gmail.com>
2025-11-11 22:06:29 +0300

    Вопрос по удалению контакта

--- addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
@@ -7,7 +7,7 @@ public class GroupRemovalTests extends TestBase {
 
     @Test
     public void canRemoveGroup() {
-        if (app.groups().isGroupPresent()) {
+        if (!app.groups().isGroupPresent()) {
             app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
         }
         app.groups().removeGroup();
