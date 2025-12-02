1ee401278a64f242b7b1093ebe17acf7e95179a9
Anton Savinykh <freevolnost@gmail.com>
2025-12-02 20:37:36 +0300

    Лекция 6.4. Проблемы

--- addressbook_web_tests/src/test/java/model/ContactData.java
+++ addressbook_web_tests/src/test/java/model/ContactData.java
@@ -24,5 +24,4 @@ public record ContactData(String id, String first_name, String last_name, String
     public ContactData withPhoto(String photo) {
         return new ContactData(this.id, this.first_name, this.last_name, this.phone, photo);
     }
-
 }
--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -73,5 +73,22 @@ public void canCreateMultipleContacts(ContactData contact) {
                 .withPhoto(randomFile("src/test/resources/images"));
         app.contact().createContact(contact);
     }
+
+    @Test
+    void canCreateContactInGroup() {
+        var contact = new ContactData()
+                .withFirstName("ТЕСТ АВАТАР")
+                .withLastName(CommonFunctions.randomString(10))
+                .withPhoto(randomFile("src/test/resources/images"));
+        if (app.hbm().getGroupCount() == 0) {
+            app.hbm().createGroup(new GroupData("", "group name", "group header", "group footer"));
+        }
+        var group = app.hbm().getGroupList().get(0);
+
+        var oldRelated = app.hbm().getContactsInGroup(group);
+        app.contact().createContact(contact, group);
+        var newRelated = app.hbm().getContactsInGroup(group);
+        Assertions.assertEquals(oldRelated.size() + 1, newRelated.size());
+    }
 }
 
